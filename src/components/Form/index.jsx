fc8b58e545e85e3d0910db35c99e8edd6edd8602
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-06-07 17:12:09 -0300

    done form style

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,10 +2,30 @@ import styles from './Form.module.css'
 
 function Form() {
     return (
-        <section className={styles.form}>
-            <form>
-                <input type="text" name='nome' placeholder='Seu nome'/>
+        <section className={styles.formArea}>
+            <form className={styles.form}>
+                <div className={styles.input}>
+                    <label className='label' htmlFor="nome">Digite seu Nome:</label>
+                    <input className={styles.input} type="text" name='nome' required/>
+                </div>
+
+                <div className={styles.input}>
+                    <label className='label' htmlFor="peso">Digite seu Peso:</label>
+                    <input className={styles.input} type="number" name='peso' required/>
+                </div>
+
+                <div className={styles.input}>
+                    <label className='label' htmlFor="altura">Digite sua Altura:</label>
+                    <input className={styles.input} type="number" name='altura' required/>
+                </div>
+                
+                <button className={styles.button} type="button">Calcular</button>
             </form>
+
+            <div className={styles.resultado}>
+                <p>Ramon, seu IMC é de: <span>12,75</span></p>
+                <p>Você está com <span>SOBREPESO</span></p>
+            </div>
         </section>
     )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 function Header() {
     return (
         <header className={styles.header}>
-            <h1>Calculadora de <span>IMC</span></h1>
+            <h1 className={styles.title}>Calculadora de <span>IMC</span></h1>
         </header>
     )
 }
